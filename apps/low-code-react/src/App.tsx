29c469c0aa8510d71b8f2a14a5051806416376cf
gulingjun <gulingjun@lrhealth.com>
2024-05-28 01:17:18 +0800

    react pages

--- apps/low-code-react/src/App.tsx
+++ apps/low-code-react/src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+// import { useState } from 'react'
+import { HashRouter, useRoutes } from 'react-router-dom'
+import { router } from './router'
+
+const Routers = () => useRoutes(router)
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <div className="text-[30px]">
-      <div>React</div>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
+    <HashRouter>
+      <Routers />
+    </HashRouter>
   )
 }
 
---
+++ apps/low-code-react/src/router/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Outlet, RouteObject } from 'react-router-dom'
+import Projects from '@/pages/projects'
+import Editor from '@/pages/editor'
+import Preview from '@/pages/preview'
+// import RecommendList from '../pages/home/tabs/recommendList'
+console.log({ ss: Editor() })
+export interface MyRouteObject {
+  title?: string
+}
+
+export type MyRouter = RouteObject & MyRouteObject
+
+export const router: Array<MyRouter> = [
+  {
+    path: '/',
+    element: <Projects />,
+    title: '项目列表'
+    // children: [
+    // 	{ path: '', element: <RecommendList /> },
+    // 	// { path: '', element: <div>推荐Tab</div> },
+    // 	{ path: 'follow', element: <div>关注Tab</div> },
+    // 	{ path: 'hot', element: <div>热榜</div> },
+    // 	{ path: 'zvideo', element: <div>视频</div> }
+    // ]
+  },
+  {
+    path: '/editor',
+    element: <Editor />,
+    title: '编辑器'
+    // children: [{ path: 'learning', element: <div>教育</div> }]
+  },
+  {
+    path: '/preview',
+    element: <Preview />,
+    title: '预览'
+  }
+]
