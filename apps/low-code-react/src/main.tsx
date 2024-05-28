d476b5b35fa26b9d6d999faadf47dfe36f079b72
gulingjun <gulingjun@lrhealth.com>
2024-05-28 15:03:15 +0800

    low-code-react

--- apps/low-code-react/src/main.tsx
+++ apps/low-code-react/src/main.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
+import App from './App.tsx'
 
 import '@dodomogu/dev-configs/tailwind.css'
-
-function App() {
-  return <div>ssss333ss</div>
-}
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <App></App>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
--- apps/low-code-react/src/pages/projects/index.tsx
+++ apps/low-code-react/src/pages/projects/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Projects: React.FC = () => {
-  return <div className="text-[36px]">Projects</div>
+  return <div className="text-[30px]">Projects</div>
 }
 
 export default Projects
