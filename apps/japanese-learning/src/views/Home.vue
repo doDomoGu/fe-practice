914fd26aa993ef2245fa44fff941fd9fd2fd889a
dodomogu <dodomo_gu@sina.cn>
2024-06-10 09:42:54 +0800

    jp-l: home

--- apps/japanese-learning/src/views/Home.vue
+++ apps/japanese-learning/src/views/Home.vue
@@ -1,5 +1,14 @@
 <script setup></script>
-
 <template>
-  <div>首页</div>
+  <div>
+    <div class="text-center">首页</div>
+    <div class="pl-[200px]">
+      <router-link
+        :to="{ name: 'fifty-tone' }"
+        class="cursor-pointer hover:text-blue-300 hover:underline"
+      >
+        五十音图>>
+      </router-link>
+    </div>
+  </div>
 </template>
