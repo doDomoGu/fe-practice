2fc8310847a170fe42e718a3ec6c2a75d371bc5e
gulingjun <gulingjun@lrhealth.com>
2024-06-13 17:27:09 +0800

    low-code_react

--- apps/low-code/react/src/pages/editor/index.tsx
+++ apps/low-code/react/src/pages/editor/index.tsx
@@ -1,8 +1,9 @@
 // import React from 'react'
+import { useParams } from 'react-router-dom'
 
-// const Editor = () => {
-function Editor() {
-  return <div>Editor</div>
+const Editor = () => {
+  const { id } = useParams<{ id: string }>()
+  return <div>Editor: {id}</div>
 }
 
 export default Editor
--- apps/low-code/react/src/pages/projects/index.tsx
+++ apps/low-code/react/src/pages/projects/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Projects: React.FC = () => {
-  return <div className="text-[30px]">Projects</div>
+const Projects = () => {
+  return (
+    <div className="h-full w-full flex flex-col">
+      <div className="text-xl font-bold text-white m-4">页面列表</div>
+      <div className="flex flex-wrap justify-start gap-4 mx-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => {
+          return (
+            <Link
+              key={id}
+              to={`/editor/${id}`}
+              className="bg-white text-black text-[30px] rounded-md w-[100px] h-[100px] cursors-pointer hover:opacity-70 text-center leading-[100px]"
+            >
+              p-{id}
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Projects
