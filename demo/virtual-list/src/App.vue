a58af3ecd27b4bf6fd23767b8fda379f1a8fca9f
gulingjun <gulingjun@lrhealth.com>
2024-07-18 17:46:24 +0800

    add demo virtual-list

---
+++ demo/virtual-list/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import List from '@/components/list/index.vue'
+import listData from './listData.js'
+
+
+const data = ref([])
+
+setTimeout(()=>{
+  data.value = listData
+},500)
+</script>
+
+<template>
+  <div>
+    <List :data="data" />
+  </div>
+</template>
+
+<style scoped>
+</style>
