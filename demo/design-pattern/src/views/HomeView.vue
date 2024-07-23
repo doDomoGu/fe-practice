1bbf44d175896dbf27afb04b9cb4c577419c609c
dodomogu <dodomo_gu@sina.cn>
2024-07-23 10:53:12 +0800

    demo: 设计模式

--- demo/design-pattern/src/views/HomeView.vue
+++ demo/design-pattern/src/views/HomeView.vue
@@ -1,4 +1,15 @@
 <script setup>
+function CallLevel() {
+  if (CallLevel.caller == null)
+    alert('CallLevel was called from the top level.')
+  else alert('CallLevel was called by another function:\n' + CallLevel.caller)
+}
+function funCaller() {
+  CallLevel()
+}
+CallLevel()
+// funCaller()
+  
 </script>
 
 <template>
