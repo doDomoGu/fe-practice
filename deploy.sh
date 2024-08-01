76ed00422b637ec5ceb6fb893177772fe8554863
dodomogu <dodomo_gu@sina.cn>
2024-08-01 17:08:22 +0800

    构建脚本 增加 dodmogu.com-blog

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ git pull
 
 
 echo '== 开始项目构建'
+
+echo '** @dodomogu/demo_animation'
 pnpm -F @dodomogu/demo_animation build
 
+echo '** @dodomogu/dodomogu.com-blog'
+pnpm -F @dodomogu/dodomogu.com-blog docs:build
 
 echo '脚本执行完毕'
