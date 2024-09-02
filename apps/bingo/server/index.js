f444ca546f42bd8a299a5f9cf6341436081cf8d8
doDomoGu <kaka_71936410@qq.com>
2024-09-02 22:33:33 +0800

    app: bingo server add redis

--- apps/bingo/server/index.js
+++ apps/bingo/server/index.js
@@ -1,4 +1,6 @@
-const express = require('express')
+import express from 'express'
+import { createClient } from 'redis'
+
 const app = express()
 const port = 3000
 
@@ -6,6 +8,36 @@ app.get('/', (req, res) => {
   res.send('Hello bingo server!')
 })
 
+app.get('/add', async (req, res) => {
+  const redisClient = await createClient({
+    url: 'redis://:ewyMF55l@8.218.170.207:6379'
+  })
+    .on('error', (err) => console.log('Redis Client Error', err))
+    .connect()
+  await redisClient.set('bingo-test-key', 'bingo-test-value' + new Date())
+  await redisClient.disconnect()
+
+  res.send('redis-set finished')
+})
+
+app.get('/get', async (req, res) => {
+  const redisClient = await createClient({
+    url: 'redis://:ewyMF55l@8.218.170.207:6379'
+  })
+    .on('error', (err) => console.log('Redis Client Error', err))
+    .connect()
+
+  // const redisClient = await createClient({
+  //   url: 'redis://:ewyMF55l@8.218.170.207:6379'
+  // })
+  const value = await redisClient.get('bingo-test-key')
+  await redisClient.disconnect()
+
+  res.send('redis-get value:' + value)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+export default app
